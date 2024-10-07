ecd541f22510e6f1d5d626631c042f120fee502a
Damián Piñones <30808181+dpinones@users.noreply.github.com>
2024-10-07 18:10:16 -0300

    add CI (#2)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
